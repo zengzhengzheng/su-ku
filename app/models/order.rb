233c1bd463c12d18473431e21adc034edb262229
zengzheng <zengzheng2009@gmail.com>
2017-06-10 00:23:08 +0800

    add ordermailer function

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
   end
 
   def set_payment_with!(method)
-    self.update_columns(payment_method)
+    self.update_columns(payment_method: method)
   end
 
   def pay!
