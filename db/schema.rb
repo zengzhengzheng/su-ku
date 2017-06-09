513627dbed2764f1250e94e432634c3df02872f6
zengzheng <zengzheng2009@gmail.com>
2017-06-09 17:08:24 +0800

    add order controller

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,8 @@ ActiveRecord::Schema.define(version: 20170609083135) do
   create_table "orders", force: :cascade do |t|
     t.integer  "total",            default: 0
     t.integer  "user_id"
-    t.string   "bulling_name"
+    t.string   "billing_name"
+    t.string   "billing_address"
     t.string   "shipping_name"
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
